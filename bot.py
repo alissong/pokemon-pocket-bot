7c38262fc2b8612c9e2f995b24a2e3f2f0c2f040
NomadCHK <nomadrchk@gmail.com>
2024-11-09 08:39:36 -0300

    first turn you shouldn't attach an energy
    Co-authored-by: Adrià Gual <adriagual06@gmail.com>

--- bot.py
+++ bot.py
@@ -201,6 +201,7 @@ class PokemonBot:
             return False
         self.log_callback("Start playing my turn...")
 
+    if not is_first_turn:
         self.add_energy_to_pokemon()
 
         ## Check playable cards (main field or bench is empty)
