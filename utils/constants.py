f4e203ed1af7da64e7f7c8e1e7de3a19fd5b83d4
NomadCHK <nomadrchk@gmail.com>
2024-11-13 01:51:32 -0300

    Adjust card offset for improved layout consistency

--- utils/constants.py
+++ utils/constants.py
@@ -8,7 +8,7 @@ default_pokemon_stats = {
 
 bench_positions = [(230, 1220), (460, 1220), (700, 1220)]
 
-card_offset_mapping = {2: 90, 3: 85, 4: 75, 5: 65, 6: 55, 7: 45, 8: 40}
+card_offset_mapping = {2: 90, 3: 87, 4: 75, 5: 65, 6: 55, 7: 45, 8: 40}
 
 ZOOM_CARD_REGION = (80, 255, 740, 1020)
 NUMBER_OF_CARDS_REGION = (790, 1325, 60, 50)
