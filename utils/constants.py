a4bf4cdcc5fe97eacc91e447b1702295859b1f82
NomadCHK <nomadrchk@gmail.com>
2024-11-15 19:19:34 -0300

    Update card offset mapping for improved alignment

--- utils/constants.py
+++ utils/constants.py
@@ -8,7 +8,15 @@ default_pokemon_stats = {
 
 bench_positions = [(230, 1220), (460, 1220), (700, 1220)]
 
-card_offset_mapping = {2: 90, 3: 89, 4: 77, 5: 65, 6: 55, 7: 47, 8: 40}
+card_offset_mapping = {
+    2: 90,
+    3: 89,
+    4: 72,  # changed from 4 from 75 to 72
+    5: 65,
+    6: 55,
+    7: 47,
+    8: 40,
+}
 
 ZOOM_CARD_REGION = (80, 255, 740, 1020)
 NUMBER_OF_CARDS_REGION = (790, 1325, 60, 50)
